abf079a374648ae376209c71552c5a15e20da94a
bhpm999 <mpolakov767@gmail.com>
2025-03-18 20:34:16 +0300

    добавил метод для открытия страницы ResumeWatch

--- src/main/java/com/example/vacancy_search/controllers/ResumeController.java
+++ src/main/java/com/example/vacancy_search/controllers/ResumeController.java
@@ -81,5 +81,10 @@ public class ResumeController {
                 .contentType(MediaType.parseMediaType(mimeType))
                 .body(resume.getFile());
     }
+    @GetMapping("/watch")
+    public String resumeWatch(Model model) {
+        model.addAttribute("resume", new Resume());
+        return "resumeWatch";
+    }
 
 }
