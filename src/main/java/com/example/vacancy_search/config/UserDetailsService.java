74bda6c50b057637e7a9945788bd4e6ab14b441f
bhpm999 <mpolakov767@gmail.com>
2025-03-18 21:28:45 +0300

    Убрал логирование

--- src/main/java/com/example/vacancy_search/config/UserDetailsService.java
+++ src/main/java/com/example/vacancy_search/config/UserDetailsService.java
@@ -4,6 +4,7 @@ import com.example.vacancy_search.domain.User;
 import com.example.vacancy_search.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -13,19 +14,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
-    private static final Logger logger = LoggerFactory.getLogger(UserDetailsService.class);
     @Autowired
     UserRepo userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        logger.info("Attempting to load user by username: {}", username);
         User user = userRepo.findByUsername(username);
         if (user == null) {
-            logger.error("User not found with username: {}", username);
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
-        logger.info("User found: {}", user.getUsername());
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
